d5cebd3f77509131e6532550f73e7090fe71a81e
Luamix <luizalfonso611@gmail.com>
2024-01-11 13:10:59 -0500

    add mode no-interactive and gitignore

--- prompt.c
+++ prompt.c
@@ -3,10 +3,14 @@
 int main() {
     char *in_line = NULL;
     size_t size = 0;
+    ssize_t read; 
 
     while (1) {
-        printf("Simple_Shell$ ");
-        ssize_t read = getline(&in_line, &size, stdin);
+        if (isatty(STDIN_FILENO))
+        {
+            printf("Simple_Shell$ ");
+        }
+        read = getline(&in_line, &size, stdin);
 
         if (read == -1) {
             break;
@@ -18,10 +22,6 @@ int main() {
 
         int count_words;
         char **words = tokenization(in_line, &count_words);
-
-        for (int i = 0; i < count_words; ++i) {
-            printf("Token %d: %s\n", i + 1, words[i]);
-        }
 
         for (int i = 0; i < count_words; ++i) {
             free(words[i]);
